c94ba78401ce8d5985e6aa9ceb90ef0cd987a3db
Dominic Ebere <54512581+eldominics@users.noreply.github.com>
2024-11-18 23:33:23 +0800

    edited webhooks

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -49,10 +49,16 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data;
-  const eventType = evt.type;
-  console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
-  console.log("Webhook payload:", body);
 
-  return new Response("Webhook received", { status: 200 });
+  const eventType = evt.type;
+
+  if (eventType === "user.created") {
+    const { id, username, email_addresses } = evt.data;
+    console.log(
+      "Our user details",
+      id,
+      username,
+      email_addresses[0].email_address
+    );
+  }
 }
