170bb1a21e2be142a1fb36a2515eba063a8e0540
Dominic Ebere <54512581+eldominics@users.noreply.github.com>
2024-12-01 20:13:36 +0800

    connected now

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -1,9 +1,11 @@
 "use server";
 
+import { connectDB } from "@/lib/mongoose";
 import UserModel from "@/models/user.model";
 
 export async function createUser(userParams: CreateUserParams) {
   try {
+    await connectDB();
     const newUser = await UserModel.create(userParams);
     return newUser;
   } catch (error) {
